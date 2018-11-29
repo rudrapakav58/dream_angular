e7ce07ffcb1c2b0145d87deb46af2b3acaa4fa5b
rudra <rudra@sanctusit.com>
2018-11-29 12:51:02 +0530

    add csss fonts img files

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'Bootstrap')))
+  .use(express.static(path.join(__dirname, 'css')))
+  .use(express.static(path.join(__dirname, 'fonts')))
+  .use(express.static(path.join(__dirname, 'img')))
+  .use(express.static(path.join(__dirname, 'js')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
